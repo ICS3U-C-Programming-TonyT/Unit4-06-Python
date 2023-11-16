f21156d3fb32d2c1d694ab9080baa3cfbc151d9a
Tony Tran <tony.tran1@stu.ocsb.ca>
2023-11-16 17:38:47 +0000

    Added rgb_nested_loop.py

---
+++ rgb_nested_loop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Tony Tran
+# Date: November. 14, 2023
+# This is program calculate the power of 2 of of 1 to your number.
+
+
+def main():
+    for red in range(0, 256):
+        for green in range(0, 256):
+            for blue in range(0, 256):
+                print(f"R:{red} G:{green} B:{blue}")
+
+
+if __name__ == "__main__":
+    main()
